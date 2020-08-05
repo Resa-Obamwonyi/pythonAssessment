8b0c642dbdd0de8264366e5f5ff2e237fe35c711
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-08-05 12:01:38 +0100

    created demo.py with function and class

---
+++ demo.py
@@ -0,0 +1,14 @@
+def isum(x,y):
+    return x+y
+
+
+isum_result = print(isum(5,10))
+
+
+class DemoClass:
+    def __init__(self, name):
+        self.name = name
+
+        def getName():
+            return name
+
